4693802c1fcd32299b9a80ea3a41bde0757ac2ce
MANOJKRAGY <38702501+MANOJKRAGY@users.noreply.github.com>
2020-06-12 03:22:32 +0530

    Setup app for HeroKu

--- public/js/app.js
+++ public/js/app.js
@@ -6,17 +6,18 @@
 //     console.log(data)
 //     })
 // })
-fetch('http://localhost:3000/weather?address=!').then((response) =>{
-    response.json().then((data) =>{
-    if(data.error){
-        console.log(data.error)
-    }  else{
-        console.log(data.location)
-        console.log(data.forecast)
-    }
 
-    })
-})
+// fetch('http://localhost:3000/weather?address=!').then((response) =>{
+//     response.json().then((data) =>{
+//     if(data.error){
+//         console.log(data.error)
+//     }  else{
+//         console.log(data.location)
+//         console.log(data.forecast)
+//     }
+
+//     })
+// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -30,7 +31,7 @@ weatherForm.addEventListener('submit',(e) =>{
     messageOne.textContent ='Loading....'
     messageTwo.textContent =''
 
-   fetch('http://localhost:3000/weather?address='+location).then((response) =>{
+   fetch('/weather?address='+location).then((response) =>{
     response.json().then((data) =>{
     if(data.error){
         messageOne.textContent = data.error
